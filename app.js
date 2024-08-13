d22b8b885b3ea443cf033c5086627612fd9a51ea
Aadi Kalra <caraadi8@gmail.com>
2024-08-12 21:11:22 -0700

    Project Almost Done

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(port, () => {
+    console.clear();
+    console.log(`Listening on port ${port}`);
+});
